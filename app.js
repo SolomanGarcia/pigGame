21e589f9fdc39ce7378e1f0c045d4a82b05dd3a6
Soloman Garcia <soloman.garcia@gmail.com>
2020-01-27 16:33:30 -0700

    added initial variables to appjs

--- app.js
+++ app.js
@@ -7,3 +7,25 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
+
+
